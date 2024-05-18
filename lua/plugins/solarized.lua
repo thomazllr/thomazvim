40562c80d81d86fb01d7973d8bfbd4a3108ced2f
thomazllr <guithomaz270@gmail.com>
2024-05-18 19:22:30 -0300

    thomazvim

---
+++ lua/plugins/solarized.lua
@@ -0,0 +1,13 @@
+return {
+
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = "light" -- or 'light'
+
+      vim.cmd.colorscheme("solarized")
+    end,
+  },
+}
